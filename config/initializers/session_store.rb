29dca85f9f3c6889cc9d3546e1a7ceb353362af3
App Academy Student <student@appacademy.io>
2013-08-29 11:28:13 -0700

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GistProject::Application.config.session_store :cookie_store, key: '_GistProject_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GistProject::Application.config.session_store :active_record_store
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+GistProject::Application.routes.draw do
+  resources :users, :only => [:create, :new, :show]
+  resource :session, :only => [:create, :destroy, :new]
+
+  root :to => "users#show"
+end
